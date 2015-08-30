ef4adead1f7af3d1fcda1c26df6ecc4b8c358ad5
grantb4812 <grantb4812@gmail.com>
2015-08-30 15:50:20 -0500

    changed background to viewport height

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
 	
-	
+	height = $(window).height();
+
+	$('.header-section, .about-section, .work-section, .contact-section').css("height", height);
 
 	$('.home').on('click', function(){
 		$('html, body').animate({
@@ -10,19 +12,19 @@ $(document).ready(function(){
 
 	$('.about').on('click', function(){
 		$('html, body').animate({
-    		scrollTop: $(".about-section").offset().top + 60
+    		scrollTop: $(".about-section").offset().top 
 				}, 1000);
 	});	
 
 	$('.gallery').on('click', function(){
 		$('html, body').animate({
-			scrollTop: $('.work-section').offset().top + 100
+			scrollTop: $('.work-section').offset().top 
 				}, 1000);
 	});
 
 	$('.contact').on('click', function(){
 		$('html, body').animate({
-			scrollTop: $('.contact-section').offset().top + 60
+			scrollTop: $('.contact-section').offset().top 
 				}, 1000);
 	});
 
