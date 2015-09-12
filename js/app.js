6e5c554e59c4601c7c0b7a4989fa273bf1eb9d6e
grantb4812 <grantb4812@gmail.com>
2015-09-12 18:50:55 -0500

    got carousel working and dynamically adding gallery

--- js/app.js
+++ js/app.js
@@ -1,8 +1,17 @@
 $(document).ready(function(){
 
-	$('#myCarousel').carousel('pause');
-	//appendProject(projects);
+	
+	appendProject(projects);
 
+	$('.work').on('click', function(){
+		$('.container').animate({
+			scrolltop: $(".latest-works").offset().top
+		}, 1000);		
+	});
+
+	
+
+	$('#myCarousel').carousel();
 
 });
 
@@ -12,29 +21,27 @@ $(document).ready(function(){
 
 var projects = [
 		{
-			img: "http://placehold.it/300x300",
-			p: "Project Title"
+			img: "http://placehold.it/600x300",
+			p: "Project Title 2"
 		},
 		{
-			img: "http://placehold.it/300x300",
-			p: "Project Title"
+			img: "http://placehold.it/600x300",
+			p: "Project Title 3"
 		},
 		{
-			img: "http://placehold.it/300x300",
-			p: "Project Title"
+			img: "http://placehold.it/600x300",
+			p: "Project Title 4"
 		},
 		{
-			img: "http://placehold.it/300x300",
-			p: "Project Title"
+			img: "http://placehold.it/600x300",
+			p: "Project Title 5"
 		}
 ]
 
 
-
-
 var createProject = function (projectObject) {
 
-	var project = $('.viewable-template .viewable-gallery').clone();
+	var project = $('.viewable-template .item').clone();
 
 	var pictureElement = project.find('.img-responsive');
 	var paragraphElement = project.find('.project-title');
@@ -45,13 +52,26 @@ var createProject = function (projectObject) {
 	return project;
 }	
 
+var createIndicator = function(n) {
+	
+	 var indicatorElement = $('.viewable-template .indicator').clone();
+	 indicatorElement.attr('data-slide-to', n + 1);
+
+	 return indicatorElement;
+}
+
 var appendProject = function (assembledProject) {
 
 	$.each(assembledProject, function(i, item) {
 
-			var append = createProject(item);
-			$('.gallery-row').append(append);
-			console.log(i)
+				
+			
+				var append = createProject(item);
+				$('.carousel-inner').append(append);
+				console.log(i)	
+
+				var galleryIndicator = createIndicator(i);
+				$('.carousel-indicators').append(galleryIndicator);	
 		
 	})
 
